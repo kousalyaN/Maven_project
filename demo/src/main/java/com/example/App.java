26b04822cacfd0cf0cb67673487f3db2612cbdc5
kousalyaN <98599038+kousalyaN@users.noreply.github.com>
2022-04-19 19:47:04 +0530

    Add files via upload

---
+++ demo/src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("my name is kousalya N");
+    }
+}
